4e676d88b88637f75e1ae44e11d99828b28ffdc7
bkb <swzbkb@163.com>
2025-03-22 15:51:31 +0800

    可视层加上

--- Assets/Scripts/Move/PlayerControl.cs
+++ Assets/Scripts/Move/PlayerControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,18 +12,25 @@ public class PlayerControl : MonoBehaviour
     [SerializeField, Tooltip("血量")] 
     public float maxHp;
 
+    [Header("特效Prefab")] 
+    public GameObject HurtVFX;
+    public GameObject DieVFX;
+
     private float currentHp;
+    [SerializeField,Tooltip("每Hp对应的尺寸")]private float[] Hp2size = new[] { 0.5f, 0.5f, 0.667f,1.0f };
     private CommonMessage HurtMessage;
     
     private float GetHurt_TimeInterval = 0.5f;
     private float GetHurtTimer = 0.0f;
+
+    public bool debug;
     
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         MessageCenter.AddListener(GetHurt,MESSAGE_TYPE.GET_HURT);
-        maxHp = 10.0f;
+        currentHp = maxHp;
     }
     void GetHurt(CommonMessage mag)
     {
@@ -32,10 +40,19 @@ public class PlayerControl : MonoBehaviour
         }
         currentHp -= mag.intParam;
         GetHurtTimer = GetHurt_TimeInterval;
+        
+        //整体变小
+        float size = Hp2size[Mathf.Clamp(Mathf.RoundToInt(currentHp),0,Hp2size.Length-1)];
+        transform.localScale = new Vector3(size, size, 1f);
         if (currentHp <= 0)
         {
             currentHp = 0;
+            Instantiate(DieVFX, transform.position, Quaternion.identity);
             //todo:游戏输了
+        }
+        else
+        {
+            Instantiate(HurtVFX, transform.position, Quaternion.identity);
         }
     }
 
@@ -47,4 +64,19 @@ public class PlayerControl : MonoBehaviour
         GetHurtTimer -= Time.deltaTime;
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            currentHp += 1;
+            currentHp = Mathf.Clamp(currentHp, 0, maxHp);
+            float size = Hp2size[Mathf.RoundToInt(currentHp)];
+            transform.localScale = new Vector3(size, size, 1f);
+        }
+    }
+
+    private void OnDisable()
+    {
+        MessageCenter.RemoveListener(GetHurt,MESSAGE_TYPE.GET_HURT);
+    }
 }
